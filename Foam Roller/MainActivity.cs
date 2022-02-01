c0b822a2cc400c8e6b1a7b083b93aa534b7531e3
Thomas Beguin <thom_78@hotmail.fr>
2022-02-01 22:39:44 +0100

    Add project files.

---
+++ Foam Roller/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+
+namespace Foam_Roller
+{
+    [Activity(Label = "@string/app_name", 
+        Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+
+            SetContentView(Resource.Layout.activity_main);
+            EditText UserNameText = FindViewById<EditText>(Resource.Id.UserNameText);
+            TextView ResponseText = FindViewById<TextView>(Resource.Id.ResponseText);
+            Button BtValide = FindViewById<Button>(Resource.Id.BtValide);
+
+            BtValide.Click += (sender, e) =>
+            {
+                string Response = Core.UserName.CheckUser(UserNameText.Text);
+                if (string.IsNullOrWhiteSpace(Response))
+                {
+                    ResponseText.Text = "";
+                    ResponseText.Visibility = Android.Views.ViewStates.Gone;
+                }
+                else
+                {
+                    ResponseText.Text = Response;
+                    ResponseText.Visibility = Android.Views.ViewStates.Visible;
+                }
+            };
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Foam Roller/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System;
+
+namespace Core
+{
+    public static class UserName
+    {
+        public static string CheckUser(string user)
+        {
+            if (string.IsNullOrWhiteSpace(user))
+                return ("");
+            else
+                user = user.ToLowerInvariant();
+
+            if (user.Contains("thomas") && user.Contains("beguin"))
+                return ("Bonjour patron");
+            else if (user.Contains("hugo") && user.Contains("van eeckhout"))
+                return ("Bonjour monsieur ECG");
+            else if (user.Contains("paul") && user.Contains("palmier"))
+                return ("Bonjour monsieur EMG");
+            else if (user.Contains("aymen") && user.Contains("kristou"))
+                return ("Bonjour monsieur moteur");
+            else
+                return ("Je ne vous connais pas !!");
+        }
+    }
+}
