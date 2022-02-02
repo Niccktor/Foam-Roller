c1b637d0c8c5e1b922d50a03c015ab3645002ab8
Thomas Beguin <thom_78@hotmail.fr>
2022-02-02 20:21:39 +0100

    Button divide by 3

--- Foam Roller/MainActivity.cs
+++ Foam Roller/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.Runtime;
 using AndroidX.AppCompat.App;
 using Android.Widget;
 using Android.Bluetooth;
+using Android.Util;
 
 namespace Foam_Roller
 {
@@ -16,16 +17,33 @@ namespace Foam_Roller
             base.OnCreate(savedInstanceState);
             //Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
-
+            DisplayMetrics display = new DisplayMetrics();
+            WindowManager.DefaultDisplay.GetMetrics(display);
+            
 
             SetContentView(Resource.Layout.activity_main);
+            LinearLayout layout = FindViewById<LinearLayout>(Resource.Id.linearLayout1);
             EditText UserNameText = FindViewById<EditText>(Resource.Id.UserNameText);
             TextView ResponseText = FindViewById<TextView>(Resource.Id.ResponseText);
             Button BtValide = FindViewById<Button>(Resource.Id.BtValide);
 
+            GridLayout grid = FindViewById<GridLayout>(Resource.Id.gridLayout1);
+            Button test1 = FindViewById<Button>(Resource.Id.test1);
+            Button test2 = FindViewById<Button>(Resource.Id.test2);
+            Button test3 = FindViewById<Button>(Resource.Id.test3);
+
+            test1.SetWidth(display.WidthPixels / 3);
+            test2.SetWidth(display.WidthPixels / 3);
+            test3.SetWidth(display.WidthPixels / 3);
+
+
+
+
+
+
             // Initialisation Bluetooth
             TextView BluetoothStatusText = FindViewById<TextView>(Resource.Id.BluetoothStatusText);
-            TextView test = FindViewById<TextView>(Resource.Id.test); 
+            TextView test = FindViewById<TextView>(Resource.Id.BluetoothStatusText); 
             BluetoothAdapter Bluetooth = BluetoothAdapter.DefaultAdapter;
             if (Core_Bluetooth.Bluetooth.isBluetoothSupported(Bluetooth, BluetoothStatusText))
             {
